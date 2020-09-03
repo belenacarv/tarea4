be4e3b9b1fc5aa49fd6b45ab8b1a1e5c93c451de
belenacarv <belen.armijo.v@mail.pucv.cl>
2020-09-03 00:25:21 -0400

    Se modifica el archivo 2

--- archivo2.py
+++ archivo2.py
@@ -1,2 +1,2 @@
-alumnos={'alumno1': ['Oscar', 7, 6], 'alumno2': ['Belen', 7, 7]}
-print(alumnos.get('alumno1'))
+alumnos={'alumno1': ['Oscar', 7, 6], 'alumno2': ['Belen', 7, 7], 'alumno3': ['Vicente', 4, 4], 'alumno4': ['Maximiliano', 6, 5]}
+print(alumnos.get('alumno4'))
