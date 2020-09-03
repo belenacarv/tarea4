f12bdf8d92c732b197f9226a14145124e3862bb1
belenacarv <belen.armijo.v@mail.pucv.cl>
2020-09-03 00:04:52 -0400

    Se actualiza el archivo 1

--- archivo1.py
+++ archivo1.py
@@ -4,4 +4,9 @@ suma = x+y
 resta = y-x
 print(f'La suma de {x} y {y} es {suma}')
 print(f'La resta de {y} y {x} es {resta}')
+multi = x*y
+divis = y/x
+modulo  = y%x
+print(f'La multiplicación entre {x} y {y} es {multi}')
+print(f'La división entre {y} y {x} es {divis} y el resto es {modulo}')
 
